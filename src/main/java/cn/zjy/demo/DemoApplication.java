4a5cf2ec83f6d019cde0c0c9a4038a33a63162c3
bananaZhang <jyzhang1994@163.com>
2020-04-19 20:37:17 +0800

    修改mybatis-plus

--- src/main/java/cn/zjy/demo/DemoApplication.java
+++ src/main/java/cn/zjy/demo/DemoApplication.java
@@ -1,21 +1,15 @@
 package cn.zjy.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-@EnableAutoConfiguration
-@MapperScan("cn.zjy.demo.dao")
 public class DemoApplication {
-
-    private static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        logger.info("----- Server Has Started -----");
+        log.info("----- Server Has Started -----");
     }
 }
